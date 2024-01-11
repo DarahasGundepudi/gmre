fff71939ffc74be169d170e108d3ed075ac318e5
DarahasGundepudi <124116084+DarahasGundepudi@users.noreply.github.com>
2024-01-11 09:49:18 +0530

    Call, mail icons and whatsapp integration

--- src/components/Titlebar.js
+++ src/components/Titlebar.js
@@ -1,4 +1,6 @@
 import logo from "../assets/logo.png";
+import call_icon from "../assets/call_icon.jpg"
+import email_icon from "../assets/email_icon.png"
 import "./Titlebar.css";
 
 const Titlebar = () => {
@@ -8,7 +10,19 @@ const Titlebar = () => {
         <li>
           <img src={logo} alt="" />
         </li>
+        <li>
+            <div>
+              <a href="tel:+918985748092">
+                <img src={call_icon} alt="Call Icon" width="20" height="20"/> Call Us: +918985748092
+              </a>
+              <a href="mailto:info@teis.in">
+                <img src={email_icon} alt="Call Icon" width="20" height="20"/> info@teis.in
+              </a>
+
+          </div>
+        </li>
       </ul>
+
     </nav>
   );
 };
